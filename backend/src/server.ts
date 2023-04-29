4956bb80099f9edc0a315a66797299b5243fa431
ssh-22 <ryosuke.masuda@beproud.jp>
2023-04-29 16:39:43 +0900

    不要な修正を削除

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createServer } from 'http';
 import { Server, OPEN as WebSocketOpen } from 'ws';
-// import { Configuration, OpenAIApi } from "openai";
 import { v4 as uuidv4 } from 'uuid';
 
 interface Message {
@@ -17,20 +16,6 @@ const server = createServer(app);
 server.listen(3001, () => console.log('Server listening on port 3001'));
 
 const wss = new Server({ server });
-
-// const chatGptApi = async (message: string): Promise<string> => {
-//   const configuration = new Configuration({
-//     organization: process.env.OPENAI_ORGANIZATION_ID,
-//     apiKey: process.env.OPENAI_API_KEY,
-//   })
-//   const openai = new OpenAIApi(configuration);
-//   const completion = await openai.createCompletion({
-//     model: 'text-davinci-003',
-//     prompt: message,
-//   });
-//   console.log(completion.data.choices[0].text)
-//   return completion.data.choices[0].text;
-// };
 
 const broadcastMessage = (message: Message): void => {
   wss.clients.forEach((client) => {
@@ -49,9 +34,6 @@ wss.on('connection', (ws) => {
   ws.on('message', async (messageJson: string) => {
     const message = JSON.parse(messageJson) as Message;
     console.log(`Received: ${message.content}`);
-
-    // const gptResponse = await chatGptApi(message.content);
-    // ws.send(JSON.stringify({ author: 'ChatGPT', content: gptResponse }));
 
     message.authorId = userId;
     broadcastMessage(message);
