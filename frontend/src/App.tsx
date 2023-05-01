76a077e45a30aae49fe1a6967601730a790bb460
ssh-22 <ryosuke.masuda@beproud.jp>
2023-05-01 21:31:14 +0900

    apply dvh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 import Chat from './components/Chat/Chat';
 
 const App: React.FC = () => {
-  const height = `${window.innerHeight - 56 - 25}px`;
+  // const height = `${window.innerHeight - 56 - 25}px`;
   return (
-    <div className='App' style={{ height }}>
+    <div className='App' >
+      {/* <div className='App' style={{ height }}> */}
       <Chat />
     </div>
   );
